f85b1fac8363250fed78931f181b5e71e03a88a0
Per-Arne Andersen <perara12@gmail.com>
2018-01-07 23:31:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='FlashRL',    # This is the name of your PyPI-package.
     version='1.0',                          # Update the version number for new releases
     install_requires=['numpy', 'pygame', "pyVNC", "pillow", "scipy", "easyprocess", "pyvirtualdisplay", "keras", "h5py"],
-    packages=["FlashRL.lib", "FlashRL.lib.util"],
+    packages=["FlashRL.lib", "FlashRL.lib.util", "FlashRL.contrib", "FlashRL.contrib.environments", "FlashRL.contrib.environments.multitask],
     scripts=[],
     dependency_links=[
         "git+ssh://git@github.com:UIA-CAIR/pyVNC.git"
