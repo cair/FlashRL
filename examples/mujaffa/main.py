811a05ddb25eb879a5ca3e0e91d2ff3ea3e9d620
Filipe de Avila Belbute-Peres <filipeabperes@gmail.com>
2018-01-14 20:38:20 -0200

    Modified Environment to pass current frame to on_frame()

--- examples/mujaffa/main.py
+++ examples/mujaffa/main.py
@@ -1,7 +1,7 @@
 from FlashRL import Game
 
 
-def on_frame(state, type, vnc):
+def on_frame(state, frame, type, vnc):
     print(state.shape, type)
 
 Game("mujaffa-v1.6", fps=1, frame_callback=on_frame, grayscale=True, normalized=True)
