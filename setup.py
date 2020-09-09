46f0a008c70d7b2e7ceade760ddf2b19abd5c671
Jonathan Daniel <gcagrad19@gmail.com>
2020-09-09 13:58:28 -0400

    Undo Previous Commit
    The previous commit did not fix the issue. Instead, it resulted in it assuming that FlashRL.Game was a directory.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='FlashRL',    # This is the name of your PyPI-package.
     version='1.0',                          # Update the version number for new releases
     install_requires=['numpy', 'pygame', "pyVNC", "pillow", "scipy", "easyprocess", "pyvirtualdisplay", "keras", "h5py"],
-    packages=["FlashRL.lib", "FlashRL.lib.util", "FlashRL.contrib", "FlashRL.contrib.environments", "FlashRL.contrib.environments.multitask", "FlashRL.Game"],
+    packages=["FlashRL.lib", "FlashRL.lib.util", "FlashRL.contrib", "FlashRL.contrib.environments", "FlashRL.contrib.environments.multitask"],
     package_data={
         '': ['*.swf'],
     },
